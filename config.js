bd1de4b6271271341a166971e57163be0e9c4be4
rafindrajaya <122076894+rafindrajaya@users.noreply.github.com>
2025-02-14 00:26:48 +0100

    config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Michelle Priscillia Pramajaya",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -14,8 +14,8 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        hearts: ['❤https://imgur.com/tN5VbXL', 'https://imgur.com/j2pewVE', 'https://imgur.com/0RE0NlR', 'https://imgur.com/rzOrA59', 'https://imgur.com/uEflnLn'],  // Heart emojis
+        bears: ['https://imgur.com/MRjUT9q', 'https://imgur.com/Nh3iq0e']                       // Cute bear emojis
     },
 
     // Questions and answers
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/daqbmmh2j/video/upload/v1739488994/cbayqsy4mbyeja1tao7s.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
